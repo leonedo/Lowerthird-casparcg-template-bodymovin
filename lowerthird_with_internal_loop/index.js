58f644aff40b53ac23d6d618071fd8bad809904c
leonedo <leonedo@users.noreply.github.com>
2025-12-30 19:01:05 -0300

    Refactor index.js for improved animation handling for animations without text layers

--- lowerthird_with_internal_loop/index.js
+++ lowerthird_with_internal_loop/index.js
@@ -174,7 +174,7 @@ anim.addEventListener('config_ready', function (e) {
     })
 
     //Add fonts to style
-    if (!fontsLoaded) {
+    if (!fontsLoaded && anim.renderer.data.fonts.list) {
         let fonts = anim.renderer.data.fonts.list;
         for (const font in fonts) {
             let family = fonts[font].fFamily
@@ -432,3 +432,4 @@ webcg.on('update', function () {
 
     }
 });
+
